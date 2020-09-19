ffd6786d65e29499c13922af4a9ac8988bf2bae3
zhushuoyuan6 <3521360586@qq.com>
2020-09-19 09:38:36 +0800

    添加了zsy类

---
+++ demo10/src/main/java/com/zht/github/zsy.java
@@ -0,0 +1,8 @@
+package com.zht.github;
+
+public class zsy {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+        System.out.println("测试版");
+    }
+}
