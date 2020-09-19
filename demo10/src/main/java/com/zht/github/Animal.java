73707dbe2623aca8cb7886624cecba7d89426b2e
changxin <1025036729@qq.com>
2020-09-19 09:39:15 +0800

    添加了年龄字段

--- demo10/src/main/java/com/zht/github/Animal.java
+++ demo10/src/main/java/com/zht/github/Animal.java
@@ -3,6 +3,15 @@ package com.zht.github;
 public class Animal {
     private int aid;
     private String aname;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public int getAid() {
         return aid;
