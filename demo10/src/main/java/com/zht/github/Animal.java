ce3eef89b6fe9867667a6701541b11644ad21170
changxin <1025036729@qq.com>
2020-09-19 09:14:22 +0800

    添加了一个动物类

---
+++ demo10/src/main/java/com/zht/github/Animal.java
@@ -0,0 +1,22 @@
+package com.zht.github;
+
+public class Animal {
+    private int aid;
+    private String aname;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+}
