dd91de2c6f2c232aa690acaa332dc8be58307ca7
zhaohaitian <397408662@qq.com>
2020-09-19 09:31:52 +0800

    添加了商品类

---
+++ demo10/src/main/java/com/zht/github/Goods.java
@@ -0,0 +1,17 @@
+package com.zht.github;
+
+/**
+ * @author shkstart
+ * @create 2020-09-19 9:30
+ */
+public class Goods {
+    private String goodsName;
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+}
